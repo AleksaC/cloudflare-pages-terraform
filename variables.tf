1c397b5b4d66189114b060d0bd604f1a5142b797
AleksaC <aleksacukovic1@gmail.com>
2024-09-13 01:53:32 +0200

    updates

--- variables.tf
+++ variables.tf
@@ -4,24 +4,28 @@ variable "name" {
 }
 
 variable "domain_name" {
-  type     = string
-  nullable = false
+  type        = string
+  nullable    = false
+  description = "Custom domain"
 }
 
 variable "account_id" {
-  type     = string
-  nullable = false
-  default  = ""
+  type        = string
+  nullable    = false
+  default     = ""
+  description = "CloudFlare account id. If not provided this module will try to extract it from `cloudflare_accounts` data source if it returns only one value, otherwise an error will be thrown"
 }
 
-variable "cloudflare_zone_id" {
-  type     = string
-  nullable = false
-  default  = ""
+variable "override_zone_settings" {
+  type        = bool
+  nullable    = false
+  default     = true
+  description = "Whether to make some opinionated changes to CloudFlare zone settings or keep the defaults"
 }
 
 variable "production_branch" {
-  type     = string
-  nullable = false
-  default  = "main"
+  type        = string
+  nullable    = false
+  default     = "main"
+  description = "The name of the branch that is used for the production environment"
 }
